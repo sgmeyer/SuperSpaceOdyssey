398b7f4c42adc20587289df86114aaf880da857c
Shawn Meyer <sgmeyer@gmail.com>
2013-12-28 16:32:09 -0600

    tweaked sprite coordinates

--- app/src/spritelibrary.js
+++ app/src/spritelibrary.js
@@ -9,12 +9,12 @@ function SpriteLibrary() {
   explosionImage.src = 'images/exp2_0.png';
 
   this.staticSprites = [
-    {id: 'goodGuyShip', x: 67, y: 123, width: 60, height: 65, image: shipImage},
-    {id: 'goodGuyShipInvincible', x: 67, y: 123, width: 60, height: 65, image: shipImageTransparent},
+    {id: 'goodGuyShip', x: 69, y: 125, width: 56, height: 59, image: shipImage},
+    {id: 'goodGuyShipInvincible', x: 69, y: 125, width: 56, height: 59, image: shipImageTransparent},
     {id: 'badGuyShip', x: 131, y: 128, width: 54, height: 56, image: shipImage},
     {id: 'badGuyShip2', x: 6, y: 3, width: 56, height: 43, image: shipImage},
     {id: 'badGuyShip3', x: 132, y: 4, width: 52, height: 55, image: shipImage},
-    {id: 'bullet', x: 131, y: 70, width: 20, height: 45, image: shipImage}
+    {id: 'bullet', x: 133, y: 69, width: 18, height: 45, image: shipImage}
   ];
   this.animationSprites = [
     { id: 'explosion', 
